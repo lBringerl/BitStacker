0b8855a5f20ad7e5ed3809395420d57fb0982a4c
lbringerl <lbringerl@hotmail.com>
2021-09-02 22:07:34 +0300

    rm get_current_price; add get_best_bid

--- login.py
+++ login.py
@@ -7,16 +7,68 @@ Created on Sun Aug 22 16:23:56 2021
 
 from abc import ABCMeta, abstractmethod
 import argparse
+import datetime
 import logging
+import time
 
 import bitmex
 
+import api_keys
 
-class Shell():
+
+class Shell:
     """ """
+    
+    supported_cmds = ('init_client',
+                      'get_best_bid', 
+                      'get_deposit_address',
+                      'exit')
     
     def __init__(self):
         """ """
+        self.current_controller = ManualControl()
+        self.init_client = InitClientCommand(self, self.current_controller)
+        self.get_best_bid = GetCurrentPiceCommand(
+            self, self.current_controller)
+        self.get_deposit_address = GetDepositAddressCommand(
+            self, self.current_controller)
+        self.exit = ExitCommand(self, self.current_controller)
+    
+    def run_shell(self):
+        """ """
+        logging.info('shell is running')
+        self.running = True
+        exit_code = self.init_client.execute(
+            f'--public_key {api_keys.PUBLIC_KEY} '
+            f'--private_key {api_keys.PRIVATE_KEY}')
+        while self.running:
+            self.run_cmd(input('>> '))
+    
+    def run_cmd(self, inp):
+        """ """
+        inp_split = inp.split(' ', 1)
+        cmd_name = inp_split[0]
+        unproc_args = ''
+        if len(inp_split) > 1:
+            unproc_args = inp_split[1] 
+        if cmd_name not in self.supported_cmds:
+            print(f'Command "{cmd_name}" is not supported. Supported commands'
+                  f'are: {self.supported_cmds}')
+            return 0
+        elif cmd_name == 'get_best_bid':
+            return self.get_best_bid.execute(unproc_args)
+        elif cmd_name == 'get_deposit_address':
+            return self.get_deposit_address.execute(unproc_args)
+        elif cmd_name == 'exit':
+            return self.exit.execute(unproc_args)
+    
+    def shell_print(self, data):
+        """ """
+        print(data)
+    
+    def shell_exit(self):
+        """ """
+        self.running = False
 
 
 class ManualControl():
@@ -26,27 +78,34 @@ class ManualControl():
         """ """
         self.client = None
         
-    
-    def login(self, public_key, private_key):
+    def init_client(self, public_key, private_key):
         """ """
-        logging.info('invoke login of ManualControl')
-        self.public_key = public_key
-        self.private_key = private_key
-        self.client = bitmex.bitmex(api_key=self.public_key,
-                                    api_secret=self.private_key)
+        logging.debug('invoke init_client of ManualControl')
+        self.client = bitmex.bitmex(api_key=public_key, api_secret=private_key)
     
-    def get_current_price(self):
+    def get_best_bid(self):
         """ """
-        logging.info('invoke get_current_price of ManualControl')
-        pass
+        logging.debug('invoke get_best_bid of ManualControl')
+        result = None
+        cur_timestamp = time.time()
+        cur_time = datetime.datetime.fromtimestamp(cur_timestamp,
+                                                   tz=datetime.timezone.utc)
+        result_lst, response = self.client.Quote.Quote_get(
+            symbol='XBTUSD',
+            count=1,
+            reverse=True,
+            endTime=cur_time).result()
+        if result_lst:
+            result = result_lst[0]
+        return result
     
     def get_deposit_address(self):
         """ """
-        logging.info('invoke get_deposit_address of ManualControl')
+        logging.debug('invoke get_deposit_address of ManualControl')
         deposit_addr, response = self.client.User.\
             User_getDepositAddress().result()
         return deposit_addr
-    
+
 
 class Command(metaclass=ABCMeta):
     """ """
@@ -66,81 +125,47 @@ class Command(metaclass=ABCMeta):
         pass
 
 
-class LoginCommand(Command):
+class InitClientCommand(Command):
     """ """
     
     args_names = ['--public_key', '--private_key']
     
     def execute(self, args_string: str) -> None:
         args = self.arg_parser.parse_args(args_string.split())
-        self.manual_control.login(**vars(args))
+        self.manual_control.init_client(**vars(args))
 
 
 class GetCurrentPiceCommand(Command):
     """ """
     
     def execute(self, args_string: str) -> None:
-        self.manual_control.get_current_price()
+        price = self.manual_control.get_best_bid()
+        self.shell.shell_print(f'Best bid: {price}')
 
 
 class GetDepositAddressCommand(Command):
     """ """
     
     def execute(self, args_string: str) -> None:
-        self.manual_control.get_deposit_address()
-
-
-
-class Shell:
+        addr = self.manual_control.get_deposit_address()
+        self.shell.shell_print(f'Deposit address: {addr}')
+        
+        
+class ExitCommand(Command):
     """ """
     
-    supported_cmds = ('login',
-                      'get_current_price', 
-                      'get_deposit_address')
-    
-    def __init__(self):
-        """ """
-        self.current_controller = ManualControl()
-        self.login = LoginCommand(self, self.current_controller)
-        self.get_current_price = GetCurrentPiceCommand(
-            self, self.current_controller)
-        self.get_deposit_address = GetDepositAddressCommand(
-            self, self.current_controller)
-    
-    def run_shell(self):
-        """ """
-        logging.info('shell is running')
-        while True:
-            self.run_cmd(input('>> '))
-    
-    def run_cmd(self, inp):
-        """ """
-        inp_split = inp.split(' ', 1)
-        cmd_name = inp_split[0]
-        unproc_args = ''
-        if len(inp_split) > 1:
-            unproc_args = inp_split[1] 
-        if cmd_name not in self.supported_cmds:
-            print(f'Command "{cmd_name}" is not supported. Supported commands'
-                  f'are: {self.supported_cmds}')
-            return
-        if cmd_name == 'login':
-            if len(inp_split) < 2:
-                print('login requires arguments')
-                return
-            self.login.execute(unproc_args)
-        elif cmd_name == 'get_current_price':
-            self.get_current_price.execute(unproc_args)
-        elif cmd_name == 'get_deposit_address':
-            self.get_deposit_address.execute(unproc_args)
-        
-        
+    def execute(self, args_string: str) -> None:
+        self.shell.shell_print('Exit shell')
+        self.shell.shell_exit()
+
+
 def main():
     """ """
     shell = Shell()
     shell.run_shell()
+    
+    return 0
 
 
 if __name__ == '__main__':
     main()
-
